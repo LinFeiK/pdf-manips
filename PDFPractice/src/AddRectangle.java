a5a7171175ac441387044d940c22c7817a0f1afd
Lin Fei Kang <linfeikang123@gmail.com>
2023-03-03 00:00:17 -0500

    feat: Changed the rectangle color and added a rectangle without border in AddRectangle.java

--- PDFPractice/src/AddRectangle.java
+++ PDFPractice/src/AddRectangle.java
@@ -13,12 +13,17 @@ public class AddRectangle {
 		
 		PdfCanvas canvas = new PdfCanvas(pdfDoc.addNewPage());
 		
-		canvas.setFillColor(Color.BLACK);
+		canvas.setFillColor(Color.BLUE);
 		
+		// Add a blue rectangle with a black border
 		// bottom-left x, bottom-left y, width, height
 		canvas.rectangle(200, 250, 100, 300);
-		
 		canvas.fillStroke();
+		
+		// Add a green rectangle with no border
+		canvas.setFillColor(Color.GREEN);
+		canvas.rectangle(150, 600, 100, 50);
+		canvas.fill();
 		
 		pdfDoc.close();
 		
